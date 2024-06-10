ed11053ab909e368de3dab13c5c1ead0cf8dee4c
SeifRashedd <seifrashedswe@gmail.com>
2024-06-10 20:35:20 +0300

    fix the gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:fashion_market/core/utils/app_router.dart';
+import 'package:fashion_market/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FashionMarcket());
 }
 
@@ -10,11 +16,9 @@ class FashionMarcket extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
     );
   }
 }
-
-
