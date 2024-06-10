f9d6c8b31177f68efc9052b463269c19a63b0874
SeifRashedd <seifrashedswe@gmail.com>
2024-06-10 12:31:56 +0300

    add getx packge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'Features/splash/presentation/views/widgets/splash_view.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class FashionMarcket extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
