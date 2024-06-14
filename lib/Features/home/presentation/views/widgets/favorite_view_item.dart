217d65a23f7c6843f63e78c26a0a6eec11c2c831
SeifRashedd <seifrashedswe@gmail.com>
2024-06-14 14:15:44 +0300

    finsh favoirt list item

--- lib/Features/home/presentation/views/widgets/favorite_view_item.dart
+++ lib/Features/home/presentation/views/widgets/favorite_view_item.dart
@@ -1,5 +1,6 @@
 import 'package:fashion_market/core/utils/app_images.dart';
 import 'package:fashion_market/core/utils/app_styles.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
@@ -9,46 +10,47 @@ class FavoriteViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      width: double.infinity,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: Container(
-          color: Colors.grey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 11),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 80,
-                  width: 70,
-                  child: AppImages.productItem,
-                ),
-                const Gap(15),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Roller Rabbit',
-                      style: AppStyles.styleSemiBold16,
-                    ),
-                    Text(
-                      'Vado Odelle Dress',
-                      style: AppStyles.styleSemiBold11,
-                    ),
-                    const Gap(8),
-                    Text(
-                      r'$198.00',
-                      style: AppStyles.styleSemiBold16,
-                    ),
-                  ],
-                ),
-                const Gap(70),
-                AppImages.addToCart,
-              ],
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        height: 100,
+        color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 11),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 80,
+                width: 70,
+                child: AppImages.productItem,
+              ),
+              const Gap(15),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Roller Rabbit',
+                    style: AppStyles.styleSemiBold16,
+                  ),
+                  Text(
+                    'Vado Odelle Dress',
+                    style: AppStyles.styleSemiBold11,
+                  ),
+                  const Gap(8),
+                  Text(
+                    r'$198.00',
+                    style: AppStyles.styleSemiBold16,
+                  ),
+                ],
+              ),
+              const Expanded(child: Gap(1)),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                    height: 50, width: 120, child: AppImages.addToCart),
+              ),
+            ],
           ),
         ),
       ),
