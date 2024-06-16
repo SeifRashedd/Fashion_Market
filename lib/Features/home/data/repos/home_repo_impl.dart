5a723f57578fc6c12db01468d789aabab59b5445
SeifRashedd <seifrashedswe@gmail.com>
2024-06-16 16:47:12 +0300

    add feach bages

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,28 @@
+import 'dart:math'; 
 import 'package:dartz/dartz.dart';
 import 'package:fashion_market/Features/home/data/models/Api/product_model/product_model.dart';
 import 'package:fashion_market/Features/home/data/repos/home_repo.dart';
 import 'package:fashion_market/core/errors/failure.dart';
+import 'package:fashion_market/core/utils/api_service.dart';
 
 class HomeRepoImpl implements HomeRepo{
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failuer, List<ProductModel>>> fetchBags() {
-   Dio
+  Future<Either<Failuer, List<ProductModel>>> fetchBags() async {
+  try {
+  var data = await apiService.get(endPoint: 'product?category=bages');
+  int count = data['count'];
+  log(count);
+  List<ProductModel> products =[];
+  for (var item in data['products']) {
+    products.add(ProductModel.fromJson(item));
+  }
+  return right(products);
+}  catch (e) {
+  return left(ServerFailure('error fetching'));
+}
   }
 
   @override
--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,16 +1,14 @@
 import 'package:dio/dio.dart';
 
-class ApiServer
-{
-  final _baseUrl = 'https://fashion-market-backend.onrender.com/api/v1';
-  final Dio dio;
+class ApiService {
+  final _baseUrl = 'https://fashion-market-backend.onrender.com/api/v1/';
+  final Dio _dio;
 
-  ApiServer(this.dio);
+  ApiService(this._dio);
 
-  Future<Map<String , dynamic>> get({required String endPoint}) async
-  {
-  var response = await dio.get('$_baseUrl$endPoint');
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await _dio.get('$_baseUrl$endPoint');
 
-  return response.data;
-  } 
+    return response.data;
+  }
 }
