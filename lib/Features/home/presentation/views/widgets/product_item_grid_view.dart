90a52231713e218a087c42a02b9bbc52a8fe7888
SeifRashedd <seifrashedswe@gmail.com>
2024-06-16 14:05:35 +0300

    add hive

--- lib/Features/home/presentation/views/widgets/product_item_grid_view.dart
+++ lib/Features/home/presentation/views/widgets/product_item_grid_view.dart
@@ -1,21 +1,29 @@
+
 import 'package:fashion_market/Features/home/presentation/views/widgets/product_item.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class ProductItemGridView extends StatelessWidget {
   const ProductItemGridView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 10,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        childAspectRatio: .79,
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 20,
-      ),
-      itemBuilder: (context, index) {
-        return const ProductItemHomeView();
+    return ValueListenableBuilder(
+      valueListenable: Hive.box('Favorites').listenable(),
+      builder: (context , box , child)
+      {
+       return GridView.builder(
+        itemCount: 10,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: .79,
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (context, index) {
+          return const ProductItemHomeView();
+        },
+      );
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:fashion_market/core/utils/app_router.dart';
 import 'package:fashion_market/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() async {
+Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('Favorites');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
