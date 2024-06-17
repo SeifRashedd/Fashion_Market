62cb3c9a72d24fb34e7169bdaa44f447df2663b5
SeifRashedd <seifrashedswe@gmail.com>
2024-06-17 14:15:15 +0300

    add getit

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:fashion_market/Features/home/data/repos/home_repo_impl.dart';
+import 'package:fashion_market/core/utils/api_service.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiService>()
+    ),
+  );
+} 
+
+
+class AuthRepo{}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:fashion_market/Features/home/data/repos/home_repo_impl.dart';
+import 'package:fashion_market/Features/home/presentation/manger/bags/bages_cubit.dart';
+import 'package:fashion_market/Features/home/presentation/manger/clothes/clothes_cubit.dart';
+import 'package:fashion_market/Features/home/presentation/manger/new_ariival/new_arrival_cubit.dart';
+import 'package:fashion_market/Features/home/presentation/manger/shoses/shoes_cubit.dart';
+import 'package:fashion_market/Features/home/presentation/manger/watches/watches_cubit.dart';
 import 'package:fashion_market/core/utils/app_router.dart';
+import 'package:fashion_market/core/utils/service_locator.dart';
 import 'package:fashion_market/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 Future<void> main() async {
+  setup();
   await Hive.initFlutter();
   await Hive.openBox('Favorites');
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +29,28 @@ class FashionMarcket extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => NewArrivalCubit(getIt.get<HomeRepoImpl>()),
+        ),
+         BlocProvider(
+          create: (context) => BagesCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => WatchesCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => ClothesCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => ShoesCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
