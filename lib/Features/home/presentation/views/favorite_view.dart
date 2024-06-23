a83bb9071f44c20de8d1bfce8f0f470d29de74f3
SeifRashedd <seifrashedswe@gmail.com>
2024-06-23 13:40:44 +0300

    navigat to product details sfrom fav

--- lib/Features/home/presentation/views/favorite_view.dart
+++ lib/Features/home/presentation/views/favorite_view.dart
@@ -35,4 +35,3 @@ class FavoriteView extends StatelessWidget {
     );
   }
 }
-
--- lib/Features/home/presentation/views/widgets/favorite_view_body.dart
+++ lib/Features/home/presentation/views/widgets/favorite_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:fashion_market/Features/home/data/models/Api/product_model/product_model.dart';
+import 'package:fashion_market/Features/home/presentation/views/product_details_view.dart';
 import 'package:fashion_market/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -6,8 +7,9 @@ import 'package:gap/gap.dart';
 class FavoriteViewBody extends StatelessWidget {
   const FavoriteViewBody({
     super.key,
-    required this.favorites,
+    required this.favorites, 
   });
+  // final ProductModel productModel;
 
   final List<ProductModel> favorites;
   @override
@@ -19,41 +21,47 @@ class FavoriteViewBody extends StatelessWidget {
         final product = favorites[index];
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Container(
-              height: 100,
-              color: Colors.grey,
-              child: Row(
-                children: [
-                  SizedBox(
-                    child: Image.network(
-                      height: 100,
-                      width: 100,
-                      product.image,
-                      fit: BoxFit.cover,
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)
+            {
+              return ProductDetailsView(productModel: product);
+            })),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Container(
+                height: 100,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      child: Image.network(
+                        height: 100,
+                        width: 100,
+                        product.image,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  const Gap(15),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        product.name,
+                    const Gap(15),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          product.name,
+                          style: AppStyles.styleSemiBold16,
+                        ),
+                        const Gap(8),
+                      ],
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Text(
+                        '\$${product.price.toString()}',
                         style: AppStyles.styleSemiBold16,
                       ),
-                      const Gap(8),
-                    ],
-                  ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Text(
-                      '\$${product.price.toString()}',
-                      style: AppStyles.styleSemiBold16,
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
